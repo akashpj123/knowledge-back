bde5bae38d782112d48b73e8f711b750ea4fb3fa
akashpj123 <akashpj77@gmail.com>
2024-06-28 10:30:52 +0530

    file changes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/api', usermake);
 // MongoDB connection
 const dbUri = process.env.MONGODB_URI || 'mongodb+srv://akashpj77:yourpassword@knowledgehub.d6y0qhm.mongodb.net/knowledge-hub?retryWrites=true&w=majority&appName=knowledgehub';
 
-mongoose.connect(dbUri)
+mongoose.connect(dbUri,{useNewUrlParser: true, useUnifiedTopology: true,})
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
