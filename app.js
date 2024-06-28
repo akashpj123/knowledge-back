d3bf38ea143683b56614024d49d3c7785cbf20e1
akashpj123 <akashpj77@gmail.com>
2024-06-28 10:14:16 +0530

    file changes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
-import crypto from 'crypto';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import cors from 'cors';
 import mongoose from 'mongoose';
-// Import your routers
-import path from "path";
+import path from 'path';
 
+// Import your routers
 import { usermake } from './routes/user-get.js';
 
 const app = express();
+
 // Use middleware
 app.use(cors({
   origin: ["https://knowledge-hub-livid.vercel.app"],
@@ -24,22 +24,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Define routes
-
 app.use('/api', usermake);
 
 // MongoDB connection
-const db = mongoose.connection;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://akashpj77:<password>@knowledgehub.d6y0qhm.mongodb.net/knowledge-hub?retryWrites=true&w=majority&appName=knowledgehub', {
+const dbUri = process.env.MONGODB_URI || 'mongodb+srv://akashpj77:yourpassword@knowledgehub.d6y0qhm.mongodb.net/knowledge-hub?retryWrites=true&w=majority&appName=knowledgehub';
+
+mongoose.connect(dbUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
   console.log('Connected to MongoDB');
 });
+
 // Start server
 const PORT = process.env.PORT || 8025;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
 export default app;
